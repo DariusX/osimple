ad1009d40e139e9066b9828e8b738a62e8d4d005
Darius <dariuscooper@gmail.com>
2016-11-07 14:36:40 -0500

    Dump env while configuring test route

--- src/main/java/com/zerses/camelsandbox/PolicyInquiryRB.java
+++ src/main/java/com/zerses/camelsandbox/PolicyInquiryRB.java
@@ -1,5 +1,7 @@
 package com.zerses.camelsandbox;
 
+import java.util.Map;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
@@ -10,43 +12,48 @@ public class PolicyInquiryRB extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-
-    restConfiguration()
-        .component("servlet")
-        .contextPath("/")
-        .host("0.0.0.0")
-  //      .port(9000)
-        .bindingMode(RestBindingMode.json)
-        // Set up Swagger config
-        .apiContextPath("/api-doc")
-        .apiProperty("api.title", "Test API").apiProperty("api.version", "1.2.3")
-        // and enable CORS so that Swagger UI can access this and present
-        // its cool interface
-        .apiProperty("cors", "true");
-    ;
-    
-
-    rest()
-        .get("/policy/{policyId}")
-        .route()
-        .to("log:From_REST_find?showAll=true")
-        .choice()
-        .when(header("policyId").isEqualTo("111"))
-        .transform(constant("Policy # ${header.policyId}: Workers Comp - Acme Widgets"))
-        .otherwise()
-        .transform(simple("Policy # ${header.policyId}: Workers Comp - Should not see this message!!!"))
-        .inOut("direct:abc")
-        .end()
+        System.out.println("\n\n=====================================================================  Environment : Start ================ ");
+                          
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n",
+                              envName,
+                              env.get(envName));
+        }
+        System.out.println("=====================================================================  Environment : End ================ \n\n");
+        
+        
+        restConfiguration()
+            .component("servlet")
+            .contextPath("/")
+            .host("0.0.0.0")
+            // .port(9000)
+            .bindingMode(RestBindingMode.json)
+            // Set up Swagger config
+            .apiContextPath("/api-doc")
+            .apiProperty("api.title", "Test API").apiProperty("api.version", "1.2.3")
+            // and enable CORS so that Swagger UI can access this and present
+            // its cool interface
+            .apiProperty("cors", "true");
         ;
-    
-    
-    
-    from("direct:abc")
-    .transform(simple("Policy # ${header.policyId}: Sending Data to Message Broker"))
-    .inOut("activemq:queue:TEST.FOO")
-    .transform(simple("Policy # ${header.policyId}: Workers Comp - After return from Message Broker"))
-    ;
-    
+
+        rest()
+            .get("/policy/{policyId}")
+            .route()
+            .to("log:From_REST_find?showAll=true")
+            .choice()
+            .when(header("policyId").isEqualTo("111"))
+            .transform(constant("Policy # ${header.policyId}: Workers Comp - Acme Widgets"))
+            .otherwise()
+            .transform(simple("Policy # ${header.policyId}: Workers Comp - Should not see this message!!!"))
+            .inOut("direct:abc")
+            .end();
+
+        from("direct:abc")
+            .transform(simple("Policy # ${header.policyId}: Sending Data to Message Broker"))
+            .inOut("activemq:queue:TEST.FOO")
+            .transform(simple("Policy # ${header.policyId}: Workers Comp - After return from Message Broker"));
+
     }
 
 }
